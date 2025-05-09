901496645904ca968df375057e73e3e1cc2ce0e3
nikolaj1307 <nikolaj1307@live.dk>
2025-05-09 11:32:36 +0200

    Now possible to have local email and production email service

--- fullstack/server/Application/EmailOptionsExtensions.cs
+++ fullstack/server/Application/EmailOptionsExtensions.cs
@@ -12,16 +12,28 @@ public static class EmailOptionsExtensions
 
         var emailOptions = configuration.GetSection("EmailOptions").Get<EmailOptions>();
 
-        services
-            .AddFluentEmail(emailOptions.SenderEmail, emailOptions.SenderName)
-            .AddSmtpSender(new System.Net.Mail.SmtpClient
-            {
-                Host = emailOptions.Host,
-                Port = emailOptions.Port,
-                Credentials = new System.Net.NetworkCredential(emailOptions.Username, emailOptions.Password),
-                EnableSsl = true
-            });
-
+        if (emailOptions.Password == null || emailOptions.Username == null)
+        {
+            services
+                .AddFluentEmail(emailOptions.SenderEmail, emailOptions.SenderName)
+                .AddSmtpSender(new System.Net.Mail.SmtpClient
+                {
+                    Host = emailOptions.Host,
+                    Port = emailOptions.Port,
+                });
+        }
+        else
+        {
+            services
+                .AddFluentEmail(emailOptions.SenderEmail, emailOptions.SenderName)
+                .AddSmtpSender(new System.Net.Mail.SmtpClient
+                {
+                    Host = emailOptions.Host,
+                    Port = emailOptions.Port,
+                    Credentials = new System.Net.NetworkCredential(emailOptions.Username, emailOptions.Password),
+                    EnableSsl = true
+                });
+        }
         return services;
     }
     
